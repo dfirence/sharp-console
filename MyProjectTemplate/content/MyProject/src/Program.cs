d3a31bc401478466264ea01a24ab43700547c103
dfirence <carlos.diaz.pro@gmail.com>
2025-11-15 18:52:39 -0500

    init structure

---
+++ MyProjectTemplate/content/MyProject/src/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MyProject;
+
+using System;
+using CommandLine;
+
+public static class Program
+{
+    private static readonly string s_dashes = new string('=', 64);
+
+    private static readonly string s_usage = $@"
+    carlos_diaz | @dfirence
+    {s_dashes}
+    Usage:
+      myproject [options]
+
+    Options:
+      --help         Show this help message
+    ";
+
+    private static void ShowHelp()
+    {
+        Console.WriteLine(s_usage);
+        Environment.Exit(0);
+    }
+
+    public class Options
+    {
+        // Define CLI options
+        [Option("help", HelpText = "Display help information.")]
+        public bool Help { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            ShowHelp();
+        }
+
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(opts =>
+            {
+                if (opts.Help)
+                {
+                    ShowHelp();
+                }
+                else
+                {
+                    Console.WriteLine("No name provided. Use --help for usage.");
+                }
+            })
+            .WithNotParsed(_ =>
+            {
+                ShowHelp();
+            });
+    }
+}
---
+++ MyProjectTemplate/content/README.md
@@ -0,0 +1,26 @@
+# MyProject Overview
+
+<br />
+
+## MyProject Structre
+### Core Implementation
+The core implementation is located here:
+
+```text
+MyProject
+├── MyProject.csproj
+└── src
+    └── Program.cs
+```
+
+<br/>
+
+
+### Unit Testing
+The unit testing will be under `tests`:
+
+```text
+tests/MyProject.Tests
+├── MyProject.Tests.csproj
+└── UnitTest1.cs
+```
